c6839890d0905e4524dff478d42f2c5cef69182e
olliecomet <leyechi@gmail.com>
2019-11-05 15:47:02 -0800

    api call gets artists in container

--- src/components/containers/SearchArtistPage.js
+++ src/components/containers/SearchArtistPage.js
@@ -1,2 +1,41 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Search from '../searches/Search';
+import PagingButtons from '../paging/PagingButtons';
+import Artists from '../artists/Artists';
+import getArtists from '../services/getArtists';
 
+export default class SearchArtistPage extends Component {
+  static propTypes = {
+
+  }
+
+  state = {
+    searchInput: '',
+    artists: []
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchInput: event.target.value });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    getArtists(this.state.searchInput, 0)
+      .then(result => this.setState({ artists: result }));
+  }
+
+  render() {
+    return (
+      <>
+        <Search
+          searchInput={this.state.searchInput}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <PagingButtons />
+        <Artists items={this.state.artists}/>
+      </>
+    );
+  }
+}
--- src/components/services/getArtists.js
+++ src/components/services/getArtists.js
@@ -1,4 +1,4 @@
-const getArtists = ({ name, page }) => {
+const getArtists = (name, page) => {
   return fetch(`http://musicbrainz.org/ws/2/artist?query=${name}&fmt=json&limit=10&offset=${10 * page}`)
     .then(res => res.json())
     .then(results => results.artists.map(({ id, name, country, disambiguation }) => ({
