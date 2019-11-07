4c51626b7a4ebe80ca88bdda562c822935a37996
olliecomet <leyechi@gmail.com>
2019-11-06 17:47:47 -0800

    lyrics css

--- src/components/lyrics/Lyric.js
+++ src/components/lyrics/Lyric.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Lyric.css';
 
 const Lyric = ({ title, lyrics }) => {
   return (
-    <section>
+    <section className={styles.Lyric}>
       <h2>{title}</h2>
       {lyrics ? <pre>{lyrics}</pre> : <p>No lyrics found :,(</p>}
     </section>
